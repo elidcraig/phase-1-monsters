2de7043a8f6879da7cd61636f6538a6c2ef15ac7
Eli Craig <elidcraig@gmail.com>
2022-05-04 12:54:00 -0700

    render monster list from fetched data

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-// Create a form within div#create-monster with submit button and 3 inputs:
-//      Name, age, and description
-// Each input should have a name, and placeholder text
-// 
+
 // When the page loads, show the first 50 monsters. Each monster's name, age, and description should be shown.
 
-// Above your list of monsters, you should have a form to create a new monster. You should have fields for name, 
-// age, and description, and a 'Create Monster Button'. When you click the button, the monster should be added to the 
-// list and saved in the API.
+// When you click the Create button, the monster should be added to the list and saved in the API.
 
 // At the end of the list of monsters, show a button. When clicked, the button should load the next 50 monsters and show them.
 
-const createMonsterContainer = document.querySelector('div#create-monster')
+const newMonsterFormContainer = document.querySelector('div#create-monster')
+const monsterListContainer = document.querySelector('div#monster-container')
 
 const renderFormElement = () => {
     const formElement = document.createElement('form')
@@ -38,12 +34,23 @@ const renderFormElement = () => {
     formElement.appendChild(descriptionInput)
     formElement.appendChild(submitInput)
 
-    createMonsterContainer.appendChild(formElement)
+    newMonsterFormContainer.appendChild(formElement)
 
+}
+
+const renderMonsterList = monstersArray => {
+    const monsterListElement = document.createElement('ul')
+    monstersArray.forEach(monsterObj => {
+        console.log(monsterObj)
+        const monster = document.createElement('li')
+        monster.textContent = `Name: ${monsterObj.name}. Age: ${monsterObj.age}. Description: ${monsterObj.description}`
+        monsterListElement.appendChild(monster)
+    })
+    monsterListContainer.appendChild(monsterListElement)
 }
 
 renderFormElement()
 
-fetch('http://localhost:3000/monsters')
+fetch('http://localhost:3000/monsters/?_limit=50')
     .then(resp => resp.json())
-    .then(monstersArray => console.log(monstersArray))
+    .then(monstersArray => renderMonsterList(monstersArray))
